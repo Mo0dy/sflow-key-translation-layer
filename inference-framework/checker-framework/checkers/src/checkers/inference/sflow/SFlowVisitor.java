a7d7b7b7fdc2a58f2a56db29f6981635cee71120
Felix Mühlenberend <felix.muehlenberend@mailbox.org>
2023-07-31 11:19:07 +0200

    Output java source files after visiting

--- inference-framework/checker-framework/checkers/src/checkers/inference/sflow/SFlowVisitor.java
+++ inference-framework/checker-framework/checkers/src/checkers/inference/sflow/SFlowVisitor.java
@@ -1,9 +1,60 @@
 package checkers.inference.sflow;
 
 import com.sun.source.tree.CompilationUnitTree;
+import com.sun.source.util.TreePath;
+import kit.edu.translation.tools.TranslationPrinter;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 
 public class SFlowVisitor extends SFlowBaseVisitor {
+    // @Felix: @TODO: Get this from the command line
+    private final String TEMP_DIR = "/home/felix/Downloads/sflowtranslationlayer";
+    private final String BASE_PATH = "/home/felix/_Uni/BA/Java_Tests";
+
     public SFlowVisitor(SFlowChecker checker, CompilationUnitTree root) {
         super(checker, root);
+    }
+
+    @Override
+    public void visit(TreePath path) {
+        this.scan(path, null);
+        this.create_key_file(path);
+    }
+
+    private void create_key_file(TreePath path) {
+        // @Felix: Output, FileCreation
+        String originalPath = path.getCompilationUnit().getSourceFile().getName();
+
+        String relativePath = originalPath.substring(BASE_PATH.length());
+        String outputPath = TEMP_DIR + relativePath;
+        File outputFile = new File(outputPath);
+        File outputDir = outputFile.getParentFile();
+
+        try {
+            // Create the output directory if it doesn't exist
+            if (!outputDir.exists()) {
+                if (!outputDir.mkdirs()) {
+                    throw new RuntimeException("Failed to create output directory: " + outputDir);
+                }
+            }
+
+            // Create the output file if it doesn't exist
+            if (!outputFile.exists()) {
+                if (!outputFile.createNewFile()) {
+                    throw new RuntimeException("Failed to create output file: " + outputFile);
+                }
+            }
+
+            // Write the pretty-printed contents to the output file
+            PrintWriter writer = new PrintWriter(new FileWriter(outputFile));
+            TranslationPrinter pretty = new TranslationPrinter(writer, true);
+            pretty.printTreePath(path);
+            writer.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
