5cd646a4d8d44f1bbaa43ce8e52f339ff65c8b9b
Felix Mühlenberend <felix.muehlenberend@mailbox.org>
2023-08-01 10:03:07 +0200

    absolute paths in my_test.sh

--- inference-framework/checker-framework/checkers/my_test.sh
+++ inference-framework/checker-framework/checkers/my_test.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env sh
 
-binary/javai-sflow /home/felix/_Uni/BA/Java_Tests/*.java
+DIR=/home/felix/_Uni/BA/type-inference/inference-framework/checker-framework/checkers
+$DIR/binary/javai-sflow /home/felix/_Uni/BA/Java_Tests/*.java
