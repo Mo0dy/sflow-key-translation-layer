ab3fde26c501fc01ba70cddd5df9cd0c39fb738b
Felix Mühlenberend <felix.muehlenberend@mailbox.org>
2023-08-27 11:50:19 +0200

    new tests

--- inference-framework/checker-framework/checkers/my_test.sh
+++ inference-framework/checker-framework/checkers/my_test.sh
@@ -4,5 +4,6 @@ CHECKER_FRAMEWORK=/home/felix/_Uni/BA/type-inference/inference-framework/checker
 SFLOW_BIN="$CHECKER_FRAMEWORK/checkers/binary"
 QUALS="$CHECKER_FRAMEWORK/checkers/resources/edu/kit/translation/checkers"
 TESTS_DIR="$CHECKER_FRAMEWORK/checkers/tests/translation-layer"
+TEMP_DIR="/home/felix/Downloads/temp"
 
-$SFLOW_BIN/javai-sflow -Aqualspath="$QUALS" -Abasepath="$TESTS_DIR" $TESTS_DIR/*.java
+$SFLOW_BIN/javai-sflow -Aqualspath="$QUALS" -Abasepath="$TESTS_DIR" -Atempdir="$TEMP_DIR" $TESTS_DIR/*.java
